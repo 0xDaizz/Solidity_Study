66b6c83b8560618ac1d1878a098baba1d34628da
Daiz <i7epr@naver.com>
2022-05-04 05:02:09 +0900

    #1. Hello Solidity
    어떤 분 무료강의를 찾아서 내일부터 정주행하려 한다.
    
    솔리디티 강좌들이 거의다 18 19 20년도껀데 이 분은 작년 강의라서 정주행 스타트. 가보자가보자~~~

---
+++ ex1_HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+// 라이선스는 항상 언급해줘야 한다는 것 같음
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Hello{
+    string public hi = "Hello Solidity";
+}
+
+//print기능이 없어서, 컴파일 후 deploy해 확인해야 하는 것은 조금 불편한 듯.
